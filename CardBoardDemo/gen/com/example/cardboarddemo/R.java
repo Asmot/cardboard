ea780dd985bc60b9ee2bc1f0e9e3cfa2ec99a50d
zhengxiangyu <INFOLIFE11@INFOLIFE-PC11>
2014-08-26 13:43:07 +0800

    add raw file

--- CardBoardDemo/gen/com/example/cardboarddemo/R.java
+++ CardBoardDemo/gen/com/example/cardboarddemo/R.java
@@ -18,27 +18,37 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int cardboard_view=0x7f070001;
-        public static final int overlay=0x7f070002;
-        public static final int ui_layout=0x7f070000;
+        public static final int cardboard_view=0x7f080001;
+        public static final int overlay=0x7f080002;
+        public static final int ui_layout=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int common_ui=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
+        public static final int main=0x7f070000;
+    }
+    public static final class raw {
+        public static final int grid_fragment=0x7f040000;
+        public static final int image_fragment=0x7f040001;
+        public static final int image_vertex=0x7f040002;
+        public static final int light_vertex=0x7f040003;
+        public static final int simple_fragment=0x7f040004;
+        public static final int simple_image_fragment=0x7f040005;
+        public static final int simple_image_vertex=0x7f040006;
+        public static final int simple_vertex=0x7f040007;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
 }
